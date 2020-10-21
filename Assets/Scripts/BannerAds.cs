4ef2b6178cfd9950edc307a0151ee54a6a6ff56f
Skinnerblake99 <56030008+Skinnerblake99@users.noreply.github.com>
2020-10-21 02:47:33 +0100

    Added adverts
    Added in title screen & ads

---
+++ Assets/Scripts/BannerAds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class BannerAds : MonoBehaviour
+{
+    string gameId = "3871599";
+    string placementId = "3871599";
+    bool testMode = true;
+
+     IEnumerator Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+
+        while (!Advertisement.IsReady(placementId))
+            yield return null;
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_RIGHT);
+        Advertisement.Banner.Show(placementId);
+    }
+}
