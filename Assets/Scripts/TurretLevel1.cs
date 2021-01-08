a1c14ee5eff3064453567e42ab306a1dd2e4101e
Skinnerblake99 <56030008+Skinnerblake99@users.noreply.github.com>
2021-01-08 04:36:06 +0000

    Added abillity to upgrade
    Added ability and cost to upgrade towers

--- Assets/Scripts/TurretLevel1.cs
+++ Assets/Scripts/TurretLevel1.cs
@@ -11,16 +11,42 @@ public class TurretLevel1 : MonoBehaviour
     private float fireCountdown = 0f;
 
     private Transform target;
-    public float TurnSpeed = 10f;
+    public float turnSpeed = 10f;
     public Transform partToRotate;
     public string enemyTag = "enemyTag";
 
     public GameObject bulletPrefab;
     public Transform firePoint;
 
+    public int turretLvl;
+
+    public GameObject turretLVLText1;
+    public GameObject turretLVLText2;
+    public GameObject turretLVLText3;
+    public GameObject canvas;
+
+
+    public bool hasUpgraded1;
+    public bool hasUpgraded2;
+    public float firstUpgradeCost = 200f;
+    public float finalUpgradeCost = 300f;
+    float currency;
     void Start()
     {
         InvokeRepeating("UpdateTarget", 0f, 0.5f);
+        turretLvl = 1;
+        range = 15f;
+        fireRate = 1f;
+        turnSpeed = 10f;
+        hasUpgraded1 = false;
+        hasUpgraded2 = false;
+        canvas = this.gameObject.transform.GetChild(2).gameObject;
+        turretLVLText1 = canvas.transform.GetChild(0).gameObject;
+        turretLVLText2 = canvas.transform.GetChild(1).gameObject;
+        turretLVLText3 = canvas.transform.GetChild(2).gameObject;
+        turretLVLText1.SetActive(true);
+        turretLVLText2.SetActive(false);
+        turretLVLText3.SetActive(false);
     }
 
     // Update is called once per frame
@@ -30,7 +56,7 @@ public class TurretLevel1 : MonoBehaviour
             return;
         Vector3 dir = target.position - transform.position;
         Quaternion lookRotation = Quaternion.LookRotation(dir);
-        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation,lookRotation,Time.deltaTime *TurnSpeed).eulerAngles;
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation,lookRotation,Time.deltaTime *turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f,rotation.y,0f);
 
         if(fireCountdown <= 0f)
@@ -40,6 +66,19 @@ public class TurretLevel1 : MonoBehaviour
         }
 
         fireCountdown -= Time.deltaTime;
+    }
+
+     void OnMouseDown()
+    {
+        if (!hasUpgraded1 && PlayerMoney.Money >= firstUpgradeCost)
+        {
+            Level2();
+        }    
+
+        if(hasUpgraded1 &&!hasUpgraded2 && PlayerMoney.Money >= finalUpgradeCost)
+        {
+            Level3();
+        }
     }
 
     void Shoot()
@@ -77,6 +116,30 @@ public class TurretLevel1 : MonoBehaviour
         }
     }
 
+    void Level2()
+    {
+        turretLvl = 2;
+        range = 20f;
+        fireRate = 1.2f;
+        turnSpeed = 12f;
+        turretLVLText1.SetActive(false);
+        turretLVLText2.SetActive(true);
+        turretLVLText3.SetActive(false);
+        PlayerMoney.Money -= firstUpgradeCost;
+        hasUpgraded1 = true;
+    }
+    void Level3()
+    {
+        turretLvl = 3;
+        range = 30f;
+        fireRate = 1.5f;
+        turnSpeed = 15f;
+        turretLVLText1.SetActive(false);
+        turretLVLText2.SetActive(true);
+        turretLVLText3.SetActive(false);
+        PlayerMoney.Money -= finalUpgradeCost;
+        hasUpgraded2 = true;
+    }
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.blue;
