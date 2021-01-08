94c94b869a0aa67d071cd452c92957cb745cc96e
Skinnerblake99 <56030008+Skinnerblake99@users.noreply.github.com>
2021-01-08 19:43:50 +0000

    Before adding google play
    Before adding google play

---
+++ Assets/Scripts/SaveGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveGame : MonoBehaviour
+{
+    bool timetoEnd = false;
+    //Standard time set to 8 minutes
+    public float timer = 480;
+    void Start()
+    {
+        timetoEnd = false;
+    }
+
+    void Update()
+    {
+        if(timer <= 0)
+        {
+            timetoEnd = true;
+        }
+    }
+}
