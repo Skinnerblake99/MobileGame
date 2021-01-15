4a478ad701a0bece02363afbbfa67406427d9906
Skinnerblake99 <56030008+Skinnerblake99@users.noreply.github.com>
2021-01-15 20:06:01 +0000

    Made a more functional UI
    Made the user interface more functional

--- Assets/Scripts/PanelHiding.cs
+++ Assets/Scripts/PanelHiding.cs
@@ -7,19 +7,28 @@ public class PanelHiding : MonoBehaviour
     //In charge of cleaning up gamne AI
     GameObject Panel;
     GameObject closeButton;
+    public GameObject cameraMain;
+    public GameObject cameraOut;
+    public GameObject cameraMainBtnOff;
+    public GameObject cameraMainBtnOn;
     void Start()
     {
         Panel = GameObject.FindGameObjectWithTag("Panel");
         Panel.SetActive(false);
         closeButton = GameObject.FindGameObjectWithTag("closeButton");
         closeButton.SetActive(false);
+        cameraOut = GameObject.FindGameObjectWithTag("OverviewCam");
+        cameraOut.SetActive(false);
+        cameraMain = GameObject.FindGameObjectWithTag("MainCam");
+        cameraMainBtnOff = GameObject.FindGameObjectWithTag("CameraButtonOff");
+        cameraMainBtnOn = GameObject.FindGameObjectWithTag("CameraButtonOn");
+        cameraMainBtnOn.SetActive(false);
     }
 
     public void ShopOpen()
     {
         Panel.SetActive(true);
     }
-
     public void ShopClose()
     {
         Panel.SetActive(false);
@@ -31,4 +40,19 @@ public class PanelHiding : MonoBehaviour
         closeButton.SetActive(true);
     }
 
+    public void CameraMainOn()
+    {
+        cameraMain.SetActive(true);
+        cameraOut.SetActive(false);
+        cameraMainBtnOn.SetActive(false);
+        cameraMainBtnOff.SetActive(true);
+    }
+
+    public void CameraMainOff()
+    {
+        cameraOut.SetActive(true);
+        cameraMain.SetActive(false);
+        cameraMainBtnOff.SetActive(false);
+        cameraMainBtnOn.SetActive(true);
+    }
 }
