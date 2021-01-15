c59782d1c158d71c319f1353500cb05cff7fe41c
Skinnerblake99 <56030008+Skinnerblake99@users.noreply.github.com>
2021-01-15 18:17:31 +0000

    Added audio button clicks
    Added audio button clicks in

---
+++ Assets/Scripts/AudioEffects.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioEffects : MonoBehaviour
+{
+    public GameObject btnSound;
+    public float soundCountdown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnSound = GameObject.FindGameObjectWithTag("ButtonPress");
+        btnSound.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        soundCountdown += Time.deltaTime;
+        if (soundCountdown >= 0.2)
+        {
+            btnSound.SetActive(false);
+        }
+    }
+
+     public void TurnOn()
+    {
+        soundCountdown = 0;
+        btnSound.SetActive(true);
+    }
+
+}
